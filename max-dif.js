6c7376e4f8e79fd9ce58e91578fbdc84eb1a85e2
ruslanvs <ruslanvs@yahoo.com>
2018-07-20 18:56:29 -0700

    Initial commit

---
+++ max-dif.js
@@ -0,0 +1,7 @@
+// Maximum difference between two elements such that larger element appears after the smaller number
+
+let arr = [2, 3, 10, 6, 40, 8, 1]
+
+function findMaxDifference(arr) {
+    
+}
